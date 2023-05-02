2992c8beb848da0a6784a35cdaced616c9514bd2
Panagiotis Panagiotakopoulos <ppanagiotisilv@gmail.com>
2023-05-02 20:37:56 +0300

    Create dialog.js

---
+++ html/dialog.js
@@ -0,0 +1,13 @@
+//close dialog when click outside the modal
+
+dialog.addEventListener("click", e => {
+  const dialogDimensions = dialog.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialog.close()
+  }
+})
